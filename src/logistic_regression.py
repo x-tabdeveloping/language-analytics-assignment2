6e7d643f331a1c207a415ea887dd49a071a3024b
Márton Kardos <power.up1163@gmail.com>
2024-05-02 09:44:57 +0200

    Added emission tracking to scripts

--- src/logistic_regression.py
+++ src/logistic_regression.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import joblib
 import pandas as pd
+from codecarbon import EmissionsTracker
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
@@ -10,26 +11,43 @@ from sklearn.pipeline import make_pipeline
 
 
 def main():
-    out_path = Path("out")
-    model_path = Path("models")
-    out_path.mkdir(exist_ok=True)
-    model_path.mkdir(exist_ok=True)
+    emissions_dir = Path("emissions")
+    emissions_dir.mkdir(exist_ok=True)
+    with EmissionsTracker(
+        project_name="fake_news_classification",
+        save_to_file=True,
+        output_file="emissions.csv",
+        output_dir=emissions_dir,
+    ) as tracker:
+        out_path = Path("out")
+        model_path = Path("models")
+        out_path.mkdir(exist_ok=True)
+        model_path.mkdir(exist_ok=True)
 
-    print("Loading data")
-    data = pd.read_csv("data/fake_or_real_news.csv")
-    X_train, X_test, y_train, y_test = train_test_split(
-        data["text"], data["label"], test_size=0.2, random_state=42
-    )
+        tracker.start_task("prepare_data")
+        print("Loading data")
+        data = pd.read_csv("data/fake_or_real_news.csv")
+        X_train, X_test, y_train, y_test = train_test_split(
+            data["text"], data["label"], test_size=0.2, random_state=42
+        )
+        tracker.stop_task()
 
-    print("Fitting classifier.")
-    classifier = make_pipeline(
-        TfidfVectorizer(stop_words="english"), LogisticRegression()
-    )
-    classifier.fit(X_train, y_train)
+        tracker.start_task("training_logistic_regression")
+        print("Fitting classifier.")
+        classifier = make_pipeline(
+            TfidfVectorizer(stop_words="english"), LogisticRegression()
+        )
+        classifier.fit(X_train, y_train)
+        tracker.stop_task()
 
-    print("Evaluating.")
-    report = classification_report(y_test, classifier.predict(X_test))
-    print(report)
+        tracker.start_task("inference_logistic_regression")
+        print("Infering labels for test set.")
+        y_pred = classifier.predict(X_test)
+        tracker.stop_task()
+
+        print("Evaluating.")
+        report = classification_report(y_test, y_pred)
+        print(report)
 
     print("Saving model and report.")
     joblib.dump(classifier, model_path.joinpath("logistic_regression.joblib"))
--- src/neural_network.py
+++ src/neural_network.py
@@ -10,24 +10,43 @@ from sklearn.pipeline import make_pipeline
 
 
 def main():
-    out_path = Path("out")
-    model_path = Path("models")
-    out_path.mkdir(exist_ok=True)
-    model_path.mkdir(exist_ok=True)
+    emissions_dir = Path("emissions")
+    emissions_dir.mkdir(exist_ok=True)
+    with EmissionsTracker(
+        project_name="fake_news_classification",
+        save_to_file=True,
+        output_file="emissions.csv",
+        output_dir=emissions_dir,
+    ) as tracker:
+        out_path = Path("out")
+        model_path = Path("models")
+        out_path.mkdir(exist_ok=True)
+        model_path.mkdir(exist_ok=True)
 
-    print("Loading data")
-    data = pd.read_csv("data/fake_or_real_news.csv")
-    X_train, X_test, y_train, y_test = train_test_split(
-        data["text"], data["label"], test_size=0.2, random_state=42
-    )
+        tracker.start_task("prepare_data")
+        print("Loading data")
+        data = pd.read_csv("data/fake_or_real_news.csv")
+        X_train, X_test, y_train, y_test = train_test_split(
+            data["text"], data["label"], test_size=0.2, random_state=42
+        )
+        tracker.stop_task()
 
-    print("Fitting classifier.")
-    classifier = make_pipeline(TfidfVectorizer(stop_words="english"), MLPClassifier())
-    classifier.fit(X_train, y_train)
+        tracker.start_task("training_neural_network")
+        print("Fitting classifier.")
+        classifier = make_pipeline(
+            TfidfVectorizer(stop_words="english"), MLPClassifier()
+        )
+        classifier.fit(X_train, y_train)
+        tracker.stop()
 
-    print("Evaluating.")
-    report = classification_report(y_test, classifier.predict(X_test))
-    print(report)
+        tracker.start_task("inference_neural_network")
+        print("Infering labels for test set.")
+        y_pred = classifier.predict(X_test)
+        tracker.stop_task()
+
+        print("Evaluating.")
+        report = classification_report(y_test, y_pred)
+        print(report)
 
     print("Saving model and report.")
     joblib.dump(classifier, model_path.joinpath("neural_network.joblib"))
